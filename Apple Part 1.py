142cffb42b1d36706f93ee34958acec5d85b5476
zayyaminyin <64671647+zayyaminyin@users.noreply.github.com>
2020-05-01 22:24:50 -0400

    Add files via upload

---
+++ Apple Part 1.py
@@ -0,0 +1,25 @@
+# Members - Riley Atkinson, Zay Ya Min Yin, Amjad Abdallah
+
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+start = dt.datetime(2014,1,1)
+end = dt.datetime(2019,12,31)
+
+df = web.DataReader('AAPL', 'yahoo', start, end)
+
+df.to_csv('apple.csv')
+
+df = pd.read_csv('apple.csv', parse_dates=True, index_col=0)
+
+#print(df)
+
+print(df[['Open','High']].head())
+
+df['Adj Close'].plot()
+plt.show()
---
+++ Apple Part 2.py
@@ -0,0 +1,36 @@
+# Members - Riley Atkinson, Zay Ya Min Yin, Amjad Abdallah
+
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+#start = dt.datetime(2019,1,1)
+#end = dt.datetime(2019,12,31)
+
+#df = web.DataReader('APPL', 'yahoo', start, end)
+
+#df.to_csv('apple.csv')
+
+df = pd.read_csv('apple.csv', parse_dates=True, index_col=0)
+
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+df.dropna(inplace=True)   
+
+print(df.head())
+
+ax1 = plt.subplot2grid((6,1),(0,0),rowspan=5,colspan=1)
+ax2 = plt.subplot2grid((6,1),(5,0),rowspan=1,colspan=1, sharex=ax1)
+
+ax1.plot(df.index,df['Adj Close'], 'r', label='Adjusted Close')
+ax1.plot(df.index,df['100ma'], 'g', label= "100 Moving Average")
+ax1.set_title('Adjusted Close and 100 day Moving Average for Apple')
+ax1.legend()
+
+ax2.bar(df.index,df['Volume'], label = "Volume", color='b')
+ax2.legend()
+
+plt.show()
